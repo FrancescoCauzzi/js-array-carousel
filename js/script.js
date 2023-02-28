5c752ea693e38cdd326217c51b4e7563d96592a7
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-02-28 15:09:05 +0100

    feat: creazione e stilizz elementi in pagina

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// - preparare un array di path di immagini
+// - preparare gli elementi in js corrispondenti agli elementi html
+// - memorizzare una variabile di indice
+// -> alla pressione del pulsante "destra"
+//   ° aumentare il valore dell'indice di un'unità
+//   ° mostrare l'immagine alla posizione dell'array relativa al valore dell'indice
+//   ° aggiungi la classe active al cerchio relativo alla posizione dell'indice
+// -> alla pressione del pulsante "sinistra"
+//   ° diminuire il valore dell'indice di un'unità
+//   ° mostrare l'immagine alla posizione dell'array relativa al valore dell'indice
+//   ° aggiungi la classe active al cerchio relativo alla posizione dell'indice
